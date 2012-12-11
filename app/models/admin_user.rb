9070d6ab4b3ecc665ef7eaa7d9a32f5a2cd835b7
Sallyca <catherine.vladimirskaya@gmail.com>
2012-12-11 04:39:30 +0400

    admin users

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -6,4 +6,15 @@ class AdminUser < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
+
+  before_destroy :raise_if_last
+  after_create { |admin| admin.send_reset_password_instructions }
+  def password_required?
+    new_record? ? false : super
+  end
+  def raise_if_last
+    if AdminUser.count < 2
+      raise "Can't delete last admin user"
+    end
+  end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,6 +8,8 @@ Bookmate::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.action_mailer.default_url_options = { :host => 'quotemate.heroku.com' }
+
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
